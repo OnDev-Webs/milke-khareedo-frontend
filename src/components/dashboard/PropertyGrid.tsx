12ba31481cc824b7155b8fade99a1229eb45acf8
Fenilsheta <139640320+Fenilsheta@users.noreply.github.com>
2025-12-26 19:31:09 +0530

    user dashboard ui changes of 4 last pages

--- src/components/dashboard/PropertyGrid.tsx
+++ src/components/dashboard/PropertyGrid.tsx
@@ -2,10 +2,30 @@ import PropertyCard from "./PropertyCard";
 
 export default function PropertyGrid({ properties }: { properties: any[] }) {
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {properties.map((p) => (
-        <PropertyCard key={p.id} {...p} />
-      ))}
-    </div>
+    <>
+      <div className="sm:hidden -mx-4 px-4">
+        <div className="flex gap-4 overflow-x-auto snap-x snap-mandatory pb-4">
+          {properties.map((property) => (
+             <div
+              key={property.id}
+              className="
+                snap-start
+                shrink-0
+                w-[90vw]        
+                max-w-[360px]
+              "
+            >
+              <PropertyCard {...property}  />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="hidden sm:grid grid-cols-2 lg:grid-cols-3 gap-6">
+        {properties.map((property) => (
+          <PropertyCard key={property.id} {...property} />
+        ))}
+      </div>
+    </>
   );
 }

5df0c33ed66e23b1f8302dac572dd3785cfb9a2f
yugjadvani-dev <yugjadvani.dev@gmail.com>
2025-12-28 17:45:25 +0530

    remove pdp page from component to avoid nested props

--- src/app/property-details/[id]/page.tsx
+++ src/app/property-details/[id]/page.tsx
@@ -1,6 +1,17 @@
 "use client";
 
-import PDP from "@/components/home/PDP/PDP";
+import PDPAboutDeveloper from "@/components/home/PDP/about-developer/aboutDeveloper";
+import PDPAmenities from "@/components/home/PDP/aminities/PDPAmenities";
+import PDPGallery from "@/components/home/PDP/gallery/PDPGallery";
+import PDPGroupProgressStatus from "@/components/home/PDP/group-progress/groupProgressStatus";
+import PDPHighLights from "@/components/home/PDP/highlights/PDPhighlights";
+import PDPLayoutPlan from "@/components/home/PDP/layoutPlan/layoutPlan";
+import PDPNeighborhood from "@/components/home/PDP/neighborhood/PDPNeighborhood";
+import PDPHeader from "@/components/home/PDP/PDPHeader/PDPHeader";
+import PDPSections from "@/components/home/PDP/PDPSections/PDPSections";
+import PDPSupport from "@/components/home/PDP/PDPSupport/PDPSupport";
+import PDPPropertyDetails from "@/components/home/PDP/property-details/property-details";
+import PDPSimilarProjects from "@/components/home/PDP/similar-projects/similarProjects";
 import { homeService, Property } from "@/lib/api";
 import React, { useEffect, useState } from "react";
 
@@ -31,7 +42,26 @@ const fetchPropertyDetails = async (id: string) => {
 
   return (
     <>
-      <PDP />
+      <PDPGallery />
+            <PDPHeader />
+            <PDPSections />
+      
+            <div className="flex w-300 mx-auto py-6 gap-5">
+              <div className="flex flex-col gap-4">
+                <PDPPropertyDetails />
+                <PDPHighLights />
+              </div>
+              <div className="flex flex-col justify-between gap-4 ">
+                <PDPGroupProgressStatus />
+                <PDPSupport />
+              </div>
+            </div>
+      
+            <PDPAmenities />
+            <PDPLayoutPlan />
+            <PDPNeighborhood />
+            <PDPAboutDeveloper />
+            <PDPSimilarProjects />
     </>
   );
 }

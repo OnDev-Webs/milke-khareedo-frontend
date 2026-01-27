c535e2071ca7642302175d945a8af4aee47dada9
Fenilsheta <139640320+Fenilsheta@users.noreply.github.com>
2026-01-28 00:50:17 +0530

    fix calculate issue to chunks error

--- src/components/home/hero/calculateSave/calculateSave.tsx
+++ src/components/home/hero/calculateSave/calculateSave.tsx
@@ -168,22 +168,10 @@ export default function CalculateSave() {
     >
       <div className="relative z-10 mx-auto max-w-[1340px] px-4 sm:px-6 lg:px-8">
         <div className="rounded-[32px] bg-white p-2 sm:p-6 shadow-[0_20px_40px_rgba(0,0,0,0.08)]">
-          <div className="w-full flex flex-col lg:grid lg:grid-cols-2 gap-6 lg:gap-10 p-2">
-            {/* ================= LEFT SIDE ================= */}
+<div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-10 p-2">            {/* ================= LEFT SIDE ================= */}
             <div className="flex flex-col gap-4">
               {/* Circular Chart and EMI Result */}
-              <div
-                className="
-    flex flex-col
-    items-center
-    text-center
-    md:flex-row md:items-start md:text-left
-    p-4 sm:px-3 sm:py-6
-    rounded-[20px] md:rounded-md
-    bg-[#EEF4FF]
-    gap-6
-  "
-              >
+      <div className="flex flex-col md:flex-row p-4 sm:px-3 sm:py-6 rounded-[20px] md:rounded-md bg-[#EEF4FF] md:items-center gap-4 md:max-w-full overflow-hidden box-border">
                 {/* Circular Pie Chart */}
                 <div className="relative mx-auto flex-shrink-0 h-36 w-36 sm:h-44 sm:w-44 md:h-48 md:w-48">
                   <div
